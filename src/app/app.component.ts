6c3ff217f90893b437517b8757b22f4242e0f343
nachomonllor <nachomonllorc@gmail.com>
2021-03-18 21:10:16 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clase01';
+
+
+  edad1:number = 0.0;
+  edad2:number = 0.0;
+
+  CambiarTitulo()
+  {
+    this.title = "otro titulo";
+  }
+ 
+  suma:number = 0.0;
+  prom:number = 0.0;
+
+  Calcular() {
+    this.suma = +this.edad1 + +this.edad2;
+    this.prom = this.suma / 2.0;
+  }
+
+  Limpiar() {
+    this.edad1 =0;
+    this.edad2 =0;
+    this.prom = 0;
+  }
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
